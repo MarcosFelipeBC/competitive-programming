9b74bc4a95a58615a67463808d5372d36ca23b29
Marcos Felipe Belisário Costa <marcosfellipecosta@hotmail.com>
2019-08-15 14:08:05 -0300

    Create Balanço de Parênteses I (Stack).cpp

---
+++ Data Structures/Balanço de Parênteses I (Stack).cpp
@@ -0,0 +1,23 @@
+//https://www.urionlinejudge.com.br/judge/pt/problems/view/1068
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	string str;
+	while (cin >> str){
+		stack < char > s;
+		bool ans = true;
+		for (int i=0; i<str.size(); i++){
+			if(str[i] == ')'){
+				if(!s.empty() && s.top() == '(') s.pop();
+				else {
+					ans = false;
+					break;
+				}
+			}
+			else if(str[i] == '(') s.push('(');
+		}
+		while (!s.empty()) s.pop(), ans = false;
+		puts((ans) ? "correct" : "incorrect");
+	}
+}
