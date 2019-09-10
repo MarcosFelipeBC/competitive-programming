4a3a1aa1bf1a72b0074703235d341c55f0d17cd1
Marcos Felipe Belisário Costa <marcosfellipecosta@gmail.com>
2019-09-10 11:19:21 -0300

    Update K -  Poor Ramzi (DP).cpp

--- Codeforces Gym/Tishreen 2017/K -  Poor Ramzi (DP).cpp
+++ Codeforces Gym/Tishreen 2017/K -  Poor Ramzi (DP).cpp
@@ -8,32 +8,32 @@ typedef long long ll;
 ll dp[55][55];
 
 ll solve(int l, int r, const string &str){
-  if(l > r || l == r) return 1LL;
-  ll &ans = dp[l][r];
-  if(ans != -1) return ans;
-  ans = 1LL;
-  int sumL = 0;
-  for (int i=l; i<r; i++){
-    sumL += (str[i] == '1');
-    int sumR = 0;
-    for (int j = r; j > i; j--){
-      sumR += (str[j] == '1');
-      if(sumL == sumR){
-        ans = (ans + solve(i+1, j-1, str))%mod;
-      }
-
-    }
-  }
-  return ans;
+	if(l > r || l == r) return 1LL;
+	ll &ans = dp[l][r];
+	if(ans != -1) return ans;
+	ans = 1LL;
+	int sumL = 0;
+	for (int i=l; i<r; i++){
+		sumL += (str[i] == '1');
+		int sumR = 0;
+		for (int j = r; j > i; j--){
+			sumR += (str[j] == '1');
+			if(sumL == sumR){
+				ans = (ans + solve(i+1, j-1, str))%mod;
+			}
+			
+		}
+	}
+	return ans;
 }
 
 int main(){
-  int t;
-  cin >> t;
-  while (t-- > 0){
-    string str;
-    cin >> str;
-    memset(dp, -1, sizeof dp);
-    cout << solve(0, str.size()-1, str) << endl;
-  }
+	int t;
+	cin >> t;
+	while (t-- > 0){
+		string str;
+		cin >> str;
+		memset(dp, -1, sizeof dp);
+		cout << solve(0, str.size()-1, str) << endl;
+	}
 }
