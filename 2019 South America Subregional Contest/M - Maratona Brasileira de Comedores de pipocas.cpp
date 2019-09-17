8cbd83ad86f8df4a3b13c119f1b1818f1d7109cc
Marcos Felipe Belisário Costa <marcosfellipecosta@gmail.com>
2019-09-17 15:32:54 -0300

    Create M - Maratona Brasileira de Comedores de pipocas.cpp

---
+++ 2019 South America Subregional Contest/M - Maratona Brasileira de Comedores de pipocas.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 100005;
+int vet[N];
+
+int main(){
+	int n, c, t;
+	scanf("%d %d %d", &n, &c, &t);
+	int mx = 0;
+	for (int i=0; i<n; i++){
+		scanf("%d", &vet[i]);
+		mx = max(mx, vet[i]);
+	}
+	int lo = mx, hi = 1000000000, ans;
+	while (lo <= hi) {
+		int mid = (lo + hi) >> 1;
+		int sum = vet[0];
+		int cnt = 1;
+		for (int i=1; i<n; i++){
+			if(sum+vet[i] > mid){
+				sum = 0;
+				cnt++;
+			}
+			sum += vet[i];
+		}
+		if(cnt <= c) {
+			ans = mid;
+			hi = mid-1;
+		}
+		else lo = mid+1;
+	}
+	printf("%d\n", ans/t + (ans%t != 0));
+}
